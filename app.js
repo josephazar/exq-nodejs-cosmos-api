95878d611b2146a1f24bf2ccc2a20f2e3976f18c
josephazar <joseph_azar@outlook.com>
2023-10-04 01:15:13 +0200

    swagger server dynamic

--- app.js
+++ app.js
@@ -5,19 +5,27 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const cors = require('cors');
+require('dotenv').config();
+
 
 //var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 var qnaRouter = require('./routes/qnalanguage.router');
 const swaggerDocument = YAML.load('./swagger-doc.yml');
-var app = express();
+swaggerDocument.servers = [{
+  url: `${process.env.API_SERVER_URL}`
+}];
 
+var app = express();
+ 
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
