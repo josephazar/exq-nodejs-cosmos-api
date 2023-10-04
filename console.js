384bfa9a20951f2542b96d57b4428b0df5a8ca49
josephazar <joseph_azar@outlook.com>
2023-10-04 19:58:42 +0200

    deleting console.js

--- console.js
+++
@@ -1,5 +0,0 @@
-const jwt = require('jsonwebtoken');
-const secret = 'ceermotorswebapi';
-//const token = jwt.sign({ client: 'ceermotors', project: 'qnachatbotwithpva' }, secret);
-const token = jwt.sign({ sourceurl:"qnaceer" }, secret);
-console.log(token);
